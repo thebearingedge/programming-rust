e733c4ab4938737cc7ed3212684d1a7e0b31e893
Tim Davis <7432943+thebearingedge@users.noreply.github.com>
2022-07-31 14:34:37 -0700

    simplify module structure

--- ch_20/src/async-chat/src/bin/server/main.rs
+++ ch_20/src/async-chat/src/bin/server/main.rs
@@ -1,24 +1,52 @@
-use async_std::{net::TcpListener, prelude::*, task};
-use chat_lib::utils::ChatResult;
-use std::sync::Arc;
+use async_std::{
+    io::BufReader,
+    net::{TcpListener, TcpStream},
+    prelude::*,
+    sync::Arc,
+    task,
+};
+use chat_lib::{
+    utils::{self, ChatError, ChatResult},
+    FromClient, FromServer,
+};
 
-mod connection;
-mod group;
-mod group_table;
+mod chat;
 
 #[async_std::main]
 async fn main() -> ChatResult<()> {
     let address = std::env::args().nth(1).expect("Usage: server ADDRESS");
-    let chat_group_table = Arc::new(group_table::GroupTable::new());
+    let chat = Arc::new(chat::Chat::new());
     task::block_on(async {
         let listener = TcpListener::bind(address).await?;
         while let Some(socket_result) = listener.incoming().next().await {
             let socket = socket_result?;
-            let groups = chat_group_table.clone();
-            task::spawn(async {
-                connection::serve(socket, groups).await.map_err(|error| {
-                    eprintln!("Error: {}", error);
-                })
+            let chat = chat.clone();
+            task::spawn(async move {
+                let client = Arc::new(chat::Client::new(socket.clone()));
+                let mut stream =
+                    utils::read_json::<BufReader<TcpStream>, FromClient>(BufReader::new(socket));
+                while let Some(data) = stream.next().await {
+                    match data? {
+                        FromClient::Join { group_name } => {
+                            let group = chat.get_or_create_group(group_name);
+                            group.join(client.clone());
+                        }
+                        FromClient::Post {
+                            group_name,
+                            message,
+                        } => match chat.get_group(&group_name) {
+                            Some(group) => group.post(message),
+                            None => {
+                                let err = FromServer::Error(format!(
+                                    "Group '{}' does not exist",
+                                    group_name
+                                ));
+                                client.send(err).await?;
+                            }
+                        },
+                    };
+                }
+                Ok::<(), ChatError>(())
             });
         }
         Ok(())
