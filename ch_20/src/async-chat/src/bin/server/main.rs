0deeb9da0a4e23f3d0707f1ea2784ad3f9a067db
Tim Davis <7432943+thebearingedge@users.noreply.github.com>
2022-07-31 12:09:19 -0700

    initial implementation of chat server from ch20, w/ some refactoring

--- ch_20/src/async-chat/src/bin/server/main.rs
+++ ch_20/src/async-chat/src/bin/server/main.rs
@@ -1,2 +1,26 @@
+use async_std::{net::TcpListener, prelude::*, task};
+use chat_lib::utils::ChatResult;
+use std::sync::Arc;
+
+mod connection;
+mod group;
+mod group_table;
+
 #[async_std::main]
-async fn main() {}
+async fn main() -> ChatResult<()> {
+    let address = std::env::args().nth(1).expect("Usage: server ADDRESS");
+    let chat_group_table = Arc::new(group_table::GroupTable::new());
+    task::block_on(async {
+        let listener = TcpListener::bind(address).await?;
+        while let Some(socket_result) = listener.incoming().next().await {
+            let socket = socket_result?;
+            let groups = chat_group_table.clone();
+            task::spawn(async {
+                connection::serve(socket, groups).await.map_err(|error| {
+                    eprintln!("Error: {}", error);
+                })
+            });
+        }
+        Ok(())
+    })
+}
