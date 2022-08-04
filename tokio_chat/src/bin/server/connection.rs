6e08bff3af862f04803809079ff317eae32ece27
Tim Davis <7432943+thebearingedge@users.noreply.github.com>
2022-08-03 18:38:32 -0700

    fuss

--- tokio_chat/src/bin/server/connection.rs
+++ tokio_chat/src/bin/server/connection.rs
@@ -13,17 +13,21 @@ pub async fn serve(chat: Arc<Chat>, socket: TcpStream) -> ChatResult<()> {
     let mut from_client = utils::read_json(BufReader::new(inbound));
     while let Some(payload) = from_client.next().await {
         match payload? {
-            FromClient::Join { group_name } => chat.join(group_name, to_client.clone()).await,
+            FromClient::Join { group_name } => {
+                let group_member = to_client.clone();
+                chat.join(group_name, group_member).await
+            }
             FromClient::Post {
                 group_name,
                 message,
             } => match chat.post_to(&group_name, message).await {
+                Some(_) => (),
                 None => {
-                    let err = FromServer::Error(format!("Group '{}' does not exist", group_name));
+                    let message = format!("Group '{}' does not exist", group_name);
+                    let err = FromServer::Error(message);
                     let mut write = to_client.lock().await;
                     utils::write_json(&mut *write, &err).await?;
                 }
-                _ => (),
             },
         }
     }
